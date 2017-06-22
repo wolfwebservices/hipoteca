c542320fb15844bd5188f9153acaae1a38b262ed
Wolf Web Services <wolfwebservices.corp@gmail.com>
2017-06-22 16:54:43 -0500

    Initial commit
    angular shit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent ,
+    AcercaComponent ,
+    ServiciosComponent ,
+    CalculadoraComponent  ,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: InicioComponent
+      },
+
+      {
+        path: 'acerca',
+        component: AcercaComponent
+      },
+
+      {
+        path: 'servicios',
+        component: ServiciosComponent
+      },
+
+      {
+        path: 'calculadora',
+        component: CalculadoraComponent
+      },
+
+      {
+        path: 'contacto',
+        component: ContactoComponent
+      }
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
